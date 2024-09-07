a26ebcd80f1382c7b5076b10645e43265a010fd7
Josiah Shvartsakh <josiah@spireex.com>
2024-09-06 20:29:23 -0400

    Add project files.

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+// Set the date we're counting down to
+const countDownDate = new Date("Nov 1, 2024 23:59:59").getTime();
+
+// Update the countdown every second
+const countdownFunction = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "We're Live!";
+    }
+}, 1000);
